2a07ba6f82aeddb5145088ab69bb92341fa0cc1f
linuxfhy <linuxfhy@163.com>
2017-08-31 10:08:17 +0800

    add table for form align

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class NameForm(forms.Form):
-    summary = forms.CharField(max_length=100)
-    priority = forms.CharField(max_length=100)
-    urgency = forms.CharField(max_length=100)
-    current_process = forms.CharField(widget=forms.Textarea)
-    deadline = forms.CharField(max_length=100)
-    curent_state = forms.CharField(max_length=100)
+    summary = forms.CharField(label='事件概要', max_length=100)
+    priority = forms.CharField(label='重要程度', max_length=100)
+    urgency = forms.CharField(label='紧急程度', max_length=100)
+    current_process = forms.CharField(label='当前进展', widget=forms.Textarea)
+    deadline = forms.CharField(label='截止时间', max_length=100)
+    curent_state = forms.CharField(label='当前状态', max_length=100)
