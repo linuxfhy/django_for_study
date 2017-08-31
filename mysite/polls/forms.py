d2f38bd38e3a6e34cfc94e3fdc29f014412434ec
linuxfhy <linuxfhy@163.com>
2017-08-31 09:20:48 +0800

    add loop for form field

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    summary = forms.CharField(max_length=100)
+    priority = forms.CharField(max_length=100)
+    urgency = forms.CharField(max_length=100)
+    current_process = forms.CharField(widget=forms.Textarea)
+    deadline = forms.CharField(max_length=100)
+    curent_state = forms.CharField(max_length=100)
