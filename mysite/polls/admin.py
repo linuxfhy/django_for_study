8a4b98aa77c23081d651e91d28b1c61c79c74b26
linuxfhy <linuxfhy@163.com>
2017-10-27 08:30:09 +0800

    register DeviceCardModel and IssueTrackModel to admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Question
 from .models import NameModel
+from .models import DeviceCardModel
+from .models import IssueTrackModel
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(NameModel)
+admin.site.register(DeviceCardModel)
+admin.site.register(IssueTrackModel)
